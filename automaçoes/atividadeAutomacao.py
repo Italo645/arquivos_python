9aff471f53014bbe2497b8c7e63c2694c727306d
Italo645 <italoo765999@gmail.com>
2025-06-03 19:37:29 -0300

    Automaçoes

---
+++ automaçoes/atividadeAutomacao.py
@@ -0,0 +1,39 @@
+import os 
+os.system ("cls")
+
+import pyautogui 
+import time
+
+
+# pyautogui.press('win')
+# time.sleep(2)
+# pyautogui.write('chrome')
+# time.sleep(2)
+# pyautogui.press('enter')
+
+#Mais de uma tecla em conjunto
+# pyautogui.hotkey('alt', 'f4')
+
+time.sleep(1)
+pyautogui.press('win')
+time.sleep(1)
+pyautogui.write('crhome')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','shift', 'n')
+time.sleep(1)
+
+pyautogui.write('gmail.com')
+time.sleep(2)
+pyautogui.press('enter')
+
+time.sleep(3)
+pyautogui.write('italoo765999@gmail.com')
+time.sleep(4)
+pyautogui.press('enter')
+time.sleep(4)
+pyautogui.click(x=1192, y=658)
+time.sleep(4)
+pyautogui.click(x=1176, y=574)
+
